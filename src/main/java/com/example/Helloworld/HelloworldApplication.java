1d9948618d102a80f0621c99faf25c3fc1a892cb
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2022-12-12 17:03:10 +0530

    changed package structure

--- src/main/java/com/example/Helloworld/HelloworldApplication.java
+++ src/main/java/com/example/Helloworld/HelloworldApplication.java
@@ -1,4 +1,4 @@
-package com.example.Helloworld.helloworld;
+package com.example.Helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
