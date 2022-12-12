df7ec3e9e8e4c6a25ccc05494621e1145083c82f
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2022-12-12 16:55:42 +0530

    Added changes for JDK 9 +

---
+++ src/main/java/com/example/Helloworld/helloworld/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.example.Helloworld.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    String displayDefaultMessage() {
+        return "Hello World, Spring Boot!";
+    }
+    
+    @GetMapping("/hello")
+    String displayDefaultMessageWithParameter(@RequestParam String input) {
+    	if(input==null)
+    	{
+    		input="NA";
+    	}
+        return "Hello World, input is :"+input+" !!!!!";
+    }
+
+}
