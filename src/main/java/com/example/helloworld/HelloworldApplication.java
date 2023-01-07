f9d0eede0708f2a8ee0f2e2dd8a130faf6cd6b89
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2023-01-07 22:45:02 +0530

    Added changes for deployment of war file in Jetty

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -2,9 +2,19 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+/**
+ * SpringBootServletInitializer is an interface to run SpringApplication from a traditional WAR deployment.
+ * It binds Servlet, Filter and ServletContextInitializer beans from the application context to the server.
+ *
+ * The HelloworldApplication sets up the Spring Boot application.
+ * It extends from SpringBootServletInitializer so that it can be deployed as a WAR.
+ *
+ * The application can be run both by deploying the WAR on a Tomcat server and executing it as a self-executable web archive with embedded Tomcat.
+ */
 @SpringBootApplication
-public class HelloworldApplication {
+public class HelloworldApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
         SpringApplication.run(HelloworldApplication.class, args);
     }
